1a794ea2b854947bd9d57b8dc4210cba9f76bb34
wwSchrader <wwschrader@gmail.com>
2018-11-28 12:39:47 -0800

    Add user registration thunk action

--- client/src/redux/actions/User.test.tsx
+++ client/src/redux/actions/User.test.tsx
@@ -1,5 +1,11 @@
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import * as types from '../constants/ActionTypes';
 import * as actions from './user';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('User actions', () => {
   it('should create an action to set user login status', () => {
@@ -20,5 +26,41 @@ describe('User actions', () => {
     };
 
     expect(actions.setUsername(usernam)).toEqual(expectedAction);
+  });
+
+  describe('registerUser thunk action', () => {
+    afterEach(() => {
+      fetchMock.restore()
+    });
+
+    it('creates USER_LOGIN when fetching register user has been done', () => {
+      const store = mockStore({});
+      const expectedAction = [{
+        type: types.USER_LOGIN,
+        userLoginStatus: true,
+      }];
+  
+      fetchMock.postOnce('/api/users/register', {isRegistered: true});
+  
+      return store.dispatch<any>(actions.registerUser('someusername', 'someemail', 'somepassword'))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedAction);
+      });
+    });
+
+    it('create USER_LOGIN as false when fetching register user has been done', () => {
+      const store = mockStore({});
+      const expectedAction = [{
+        type: types.USER_LOGIN,
+        userLoginStatus: false,
+      }];
+
+      fetchMock.postOnce('/api/users/register', {isRegistered: false});
+
+      return store.dispatch<any>(actions.registerUser('anoterusername', 'anotheremail', 'anotherpassword'))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedAction);
+      });
+    });
   });
 });
--- client/src/redux/actions/User.tsx
+++ client/src/redux/actions/User.tsx
@@ -13,3 +13,32 @@ export function setUsername(usernam: string) {
     username:usernam
   };
 };
+
+export function registerUser(usernam: string, userEmail: string, userPassword: string) {
+  return (dispatch: any) => {
+    return fetch('/api/users/register', {
+      body: JSON.stringify({username: usernam, email: userEmail, password: userPassword}),
+      credentials: 'include',
+      headers: {'Content-Type': 'application/json'},
+      method: 'POST'
+    })
+    .then(response => {
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+
+      return response;
+    })
+    .then(resp => resp.json())
+    .then(body => {
+      if (body.isRegistered) {
+        return dispatch(isLoggedIn(true));
+      } else {
+        return dispatch(isLoggedIn(false));
+      };
+    })
+    .catch(err => {
+      return dispatch(isLoggedIn(false));
+    });
+  }
+}
