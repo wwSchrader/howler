74d53b584ec609cdc679371d4ae4eca640b14801
wwSchrader <wwschrader@gmail.com>
2018-12-01 19:32:20 -0800

    Setup file structure for components and delete template UI

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>Howler App</h1>
+      </div>
+    );
+  };
+};
+
+export default App;
