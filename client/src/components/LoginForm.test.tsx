99b0f60b71d84fffecea6ceb359475545eee5c13
wwSchrader <wwschrader@gmail.com>
2018-12-03 14:17:04 -0800

    Add form to LoginForm component

--- client/src/components/LoginForm.test.tsx
+++ client/src/components/LoginForm.test.tsx
@@ -5,10 +5,69 @@ import LoginForm from './LoginForm';
 
 describe('LoginForm', () => {
   let wrapper: ShallowWrapper;
+  let instance: LoginForm;
 
   beforeEach(() => wrapper = shallow(<LoginForm />));
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render a <Form />', () => {
+    expect(wrapper.find('Form').length).toEqual(1);
+  });
+
+  it('should render <FormGroup />', () => {
+    expect(wrapper.find('FormGroup').length).toEqual(2);
+  });
+
+  it('should render <Label />', () => {
+    expect(wrapper.find('Label').length).toEqual(2);
+  });
+
+  it('should render <Input />', () => {
+    expect(wrapper.find('Input').length).toEqual(2);
+  });
+
+  it('should render <Button />', () => {
+    expect(wrapper.find('Button').length).toEqual(1);
+  });
+
+  describe('handleUsernameChange function', () => {
+    beforeEach(() => {
+      instance = wrapper.instance() as LoginForm;
+    });
+
+    it('should change the state for username', () => {
+      const newUsername = 'newUsername';
+      wrapper.setState({username: ''});
+      instance.handleUsernameChange({target: {value: newUsername}});
+      expect(wrapper.state('username')).toEqual(newUsername);
+    });
+  });
+
+  describe('handlePasswordChange function', () => {
+    beforeEach(() => {
+      instance = wrapper.instance() as LoginForm;
+    });
+
+    it('should change the state for password', () => {
+      const newPassword = 'password123';
+      wrapper.setState({username: ''});
+      instance.handleUsernameChange({target: {value: newPassword}});
+      expect(wrapper.state('username')).toEqual(newPassword);
+    });
+  });
+
+  describe('handleSubmit function', () => {
+    let mockPreventDefault: jest.Mock;
+    beforeEach(() => {
+      instance = wrapper.instance() as LoginForm;
+      mockPreventDefault = jest.fn(() => null);
+    });
+
+    it('should handle valid submission', () => {
+      wrapper.setState({loginButtonPressed: false});
+      
+      instance.handleSubmit({preventDefault: mockPreventDefault});
+      expect(mockPreventDefault.mock.calls.length).toEqual(1);
+      expect(wrapper.state('loginButtonPressed')).toEqual(true);
+    });
   });
 });
--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,9 +1,60 @@
 import * as React from 'react';
+import {Button, Form, FormGroup, Input, Label} from 'reactstrap';
 
-class LoginForm extends React.Component {
+class LoginForm extends React.Component<{}, {password: string, username: string, loginButtonPressed: boolean}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      loginButtonPressed: false,
+      password: '',
+      username: '',
+    };
+
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  public handleUsernameChange = (e: any) => {
+    this.setState({username: e.target.value});
+  };
+
+  public handlePasswordChange = (e: any) => {
+    this.setState({password: e.target.value});
+  };
+
+  public handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.setState({loginButtonPressed: true});
+
+    if(this.state.username.length > 0 && this.state.password.length > 0) {
+      // insert redux action call here
+    };
+  };
+
   public render() {
     return (
-      <div>LoginForm</div>
+      <Form onSubmit={this.handleSubmit}>
+        <FormGroup>
+          <Label>Username</Label>
+          <Input
+            type='text'
+            placeholder='Enter your username'
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input
+            type='password'
+            placeholder='Enter your password'
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+        </FormGroup>
+        <Button type='submit'>Login</Button>
+      </Form>
     );
   };
 };
