85b096ef9831fc5191752bc259a9924a205956b2
wwSchrader <wwschrader@gmail.com>
2018-12-02 21:37:33 -0800

    Add modal to component

--- client/src/components/UserRegOrLoginModal.test.tsx
+++ client/src/components/UserRegOrLoginModal.test.tsx
@@ -1,5 +1,6 @@
 import {shallow, ShallowWrapper} from 'enzyme';
 import * as React from 'react';
+import {Button} from 'reactstrap';
 import '../setupTests';
 import UserRegOrLoginModal from './UserRegOrLoginModal';
 
@@ -8,7 +9,81 @@ describe('UserRegOrLoginModal', () => {
   
   beforeEach(() => wrapper = shallow(<UserRegOrLoginModal />));
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render a <Modal />', () => {
+    expect(wrapper.find('Modal').length).toEqual(1);
+  });
+
+  it('should render a <ModalHeader />', () => {
+    expect(wrapper.find('ModalHeader').length).toEqual(1);
+  });
+
+  it('should render a <ModalBody />', () => {
+    expect(wrapper.find('ModalBody').length).toEqual(1);
+  });
+
+  it('should render a <ModalFooter />', () => {
+    expect(wrapper.find('ModalFooter').length).toEqual(1);
+  });
+
+  it('should render a <Button />', () => {
+    expect(wrapper.find('Button').length).toEqual(1);
+  });
+
+  describe('switchStatus function', () => {
+    let instance: UserRegOrLoginModal;
+    beforeEach(() => {
+      wrapper = shallow(<UserRegOrLoginModal />)
+      instance = wrapper.instance() as UserRegOrLoginModal;
+    });
+
+    it('should change isLoginModal to true', () => {
+      wrapper.setState({isLoginModal: false});
+      instance.switchStatus();
+      expect(wrapper.state('isLoginModal')).toBe(true);
+    });
+
+    it('should change isLoginModal to false', () => {
+      wrapper.setState({isLoginModal: true});
+      instance.switchStatus();
+      expect(wrapper.state('isLoginModal')).toBe(false);
+    });
+  });
+
+  describe('decideWhichButtonToRender function', () => {
+    let instance: UserRegOrLoginModal;
+    beforeEach(() => {
+      wrapper = shallow(<UserRegOrLoginModal />)
+      instance = wrapper.instance() as UserRegOrLoginModal;
+    });
+
+    it('should return Button with text Register', () => {
+      wrapper.setState({isLoginModal: true});
+      const returnedButton = instance.decideWhichButtonToRender();
+      expect(returnedButton).toEqual(<Button onClick={instance.switchStatus}>Register</Button>);
+    });
+
+    it('should return Button with text Login', () => {
+      wrapper.setState({isLoginModal: false});
+      const returnedButton = instance.decideWhichButtonToRender();
+      expect(returnedButton).toEqual(<Button onClick={instance.switchStatus}>Login</Button>);
+    });
+  });
+
+  describe('decideModalHeaderText function', () => {
+    let instance: UserRegOrLoginModal;
+    beforeEach(() => {
+      wrapper = shallow(<UserRegOrLoginModal />)
+      instance = wrapper.instance() as UserRegOrLoginModal;
+    });
+
+    it('should return string Login', () => {
+      wrapper.setState({isLoginModal: true});
+      expect(instance.decideModalHeaderText()).toEqual('Login');
+    });
+
+    it('should return string Register', () => {
+      wrapper.setState({isLoginModal: false});
+      expect(instance.decideModalHeaderText()).toEqual('Register');
+    });
   });
 });
--- client/src/components/UserRegOrLoginModal.tsx
+++ client/src/components/UserRegOrLoginModal.tsx
@@ -1,12 +1,54 @@
 import * as React from 'react';
+import {Button, Modal, ModalBody, ModalHeader} from 'reactstrap';
+import ModalFooter from 'reactstrap/lib/ModalFooter';
 
-class UserRegOrLoginModal extends React.Component {
+class UserRegOrLoginModal extends React.Component<{}, {isLoginModal: boolean}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isLoginModal: true,
+    };
+
+    this.switchStatus = this.switchStatus.bind(this);
+    this.decideWhichButtonToRender = this.decideWhichButtonToRender.bind(this);
+    this.decideModalHeaderText = this.decideModalHeaderText.bind(this);
+  }
+
   public render() {
     return (
-      <div>
-        UserRegOrLoginModal
-      </div>
+      <Modal isOpen={true}>
+        <ModalHeader>{this.decideModalHeaderText()}</ModalHeader>
+        <ModalBody>
+          Modal Body here.
+        </ModalBody>
+        <ModalFooter>
+          {this.decideWhichButtonToRender()}
+        </ModalFooter>
+      </Modal>
     );
+  }
+
+  public switchStatus = () => {
+    this.setState({isLoginModal: !this.state.isLoginModal});
+  }
+
+  public decideWhichButtonToRender = () => {
+    let buttonText: string;
+    if(this.state.isLoginModal) {
+      buttonText = 'Register';
+    } else {
+      buttonText = 'Login';
+    }
+
+    return <Button onClick={this.switchStatus}>{buttonText}</Button>;
+  }
+
+  public decideModalHeaderText = () => {
+    if(this.state.isLoginModal) {
+      return 'Login';
+    }
+
+    return 'Register';
   }
 }
 
