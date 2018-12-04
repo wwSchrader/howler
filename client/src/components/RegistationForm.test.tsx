53281b08527d6b16f0f2fc70a16fd82cc704cb4f
wwSchrader <wwschrader@gmail.com>
2018-12-03 19:47:08 -0800

    Add form to RegistrationForm Component

--- client/src/components/RegistationForm.test.tsx
+++ client/src/components/RegistationForm.test.tsx
@@ -5,10 +5,82 @@ import RegistrationForm from './RegistrationForm';
 
 describe('RegistrationForm', () => {
   let wrapper: ShallowWrapper;
+  let instance: RegistrationForm;
 
   beforeEach(() => wrapper= shallow(<RegistrationForm />));
 
-  it('should rendera <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render <Form />', () => {
+    expect(wrapper.find('Form').length).toEqual(1);
+  });
+
+  it('should render <FormGroup />', () => {
+    expect(wrapper.find('FormGroup').length).toEqual(3);
+  });
+
+  it('should render <Label />', () => {
+    expect(wrapper.find('Label').length).toEqual(3);
+  });
+
+  it('should render <Input />', () => {
+    expect(wrapper.find('Input').length).toEqual(3);
+  });
+
+  it('should render <Button />', () => {
+    expect(wrapper.find('Button').length).toEqual(1);
+  });
+
+  describe('handleUsernameChange function', () => {
+    beforeEach(() => {
+      instance = wrapper.instance() as RegistrationForm;
+    });
+
+    it('should change the state for the username', () => {
+      const newUsername = 'newUsername';
+      wrapper.setState({username: ''});
+      instance.handleUsernameChange({target: {value: newUsername}});
+      expect(wrapper.state('username')).toEqual(newUsername);
+    });
+  });
+
+  describe('handleEmailChange function', () => {
+    beforeEach(() => {
+      instance = wrapper.instance() as RegistrationForm;
+    });
+
+    it('should change the state for the email', () => {
+      const newEmail = 'someemail@anywhere.com';
+      wrapper.setState({email: ''});
+      instance.handleEmailChange({target: {value: newEmail}});
+      expect(wrapper.state('email')).toEqual(newEmail);
+    });
+  });
+
+  describe('handlePasswordChange function', () => {
+    beforeEach(() => {
+      instance = wrapper.instance() as RegistrationForm;
+    });
+
+    it('should change the state for the password', () => {
+      const newPassword = 'password123';
+      wrapper.setState({password: ''});
+      instance.handlePasswordChange({target: {value: newPassword}});
+      expect(wrapper.state('password')).toEqual(newPassword);
+    });
+  });
+
+  describe('handleSubmit function', () => {
+    let mockPreventDefault: jest.Mock;
+    beforeEach(() => {
+      instance = wrapper.instance() as RegistrationForm;
+      mockPreventDefault = jest.fn(() => null);
+    });
+
+    it('should handle valid submission', () => {
+      wrapper.setState({registerButtonPressed: false});
+      
+      instance.handleSubmit({preventDefault: mockPreventDefault});
+      expect(mockPreventDefault.mock.calls.length).toEqual(1);
+      expect(wrapper.state('registerButtonPressed')).toEqual(true);
+    });
   });
 });
--- client/src/components/RegistrationForm.tsx
+++ client/src/components/RegistrationForm.tsx
@@ -1,13 +1,68 @@
 import * as React from 'react';
+import {Button, Form, FormGroup, Input, Label} from 'reactstrap';
 
-class RegistrationForm extends React.Component {
+class RegistrationForm extends React.Component<{}, {email: string, registerButtonPressed: boolean, username: string, password: string}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      registerButtonPressed: false,
+      username: '',
+    };
+  };
+
+  public handleUsernameChange = (e: any) => {
+    this.setState({username: e.target.value});
+  };
+
+  public handleEmailChange = (e: any) => {
+    this.setState({email: e.target.value});
+  };
+
+  public handlePasswordChange = (e: any) => {
+    this.setState({password: e.target.value});
+  };
+
+  public handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.setState({registerButtonPressed: true});
+  };
+
   public render() {
     return (
-      <div>
-        Registration Form
-      </div>
+      <Form onSubmit={this.handleSubmit}>
+        <FormGroup>
+          <Label>Username</Label>
+          <Input
+            type='text'
+            placeholder='Enter a username'
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input
+            type='email'
+            placeholder='Enter an email'
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input
+            type='password'
+            placeholder='Enter a password'
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+        </FormGroup>
+        <Button>Register</Button>
+      </Form>
     );
-  }
-}
+  };
+};
 
 export default RegistrationForm;
