09f2cef0daddbff69ff4dad60b8a87a62088fa0d
wwSchrader <wwschrader@gmail.com>
2018-12-07 20:37:13 -0800

    Add Form elements to AddTweetForm

--- client/src/components/AddTweetForm.test.tsx
+++ client/src/components/AddTweetForm.test.tsx
@@ -5,12 +5,37 @@ import {AddTweetForm} from './AddTweetForm';
 
 describe('AddTweetForm', () => {
   let wrapper: ShallowWrapper;
+  let instance: AddTweetForm;
 
   beforeEach(() => {
     wrapper = shallow(<AddTweetForm />);
   });
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render a <Form />', () => {
+    expect(wrapper.find('Form').length).toEqual(1);
+  });
+
+  it('should render a <FormGroup />', () => {
+    expect(wrapper.find('FormGroup').length).toEqual(1);
+  });
+
+  it('should render a <Input />', () => {
+    expect(wrapper.find('Input').length).toEqual(1);
+  });
+
+  it('should render a <Button />', () => {
+    expect(wrapper.find('Button').length).toEqual(1);
+  });
+
+  describe('handleOnTweetTexChange function', () => {
+    beforeEach(() => {
+      instance = wrapper.instance() as AddTweetForm;
+    });
+
+    it('should set the state of tweet to the input text', () => {
+      const testTweet = 'This is a test tweet #test';
+      instance.handleOnTweetTextChange({target: {value: testTweet}});
+      expect(wrapper.state('tweet')).toEqual(testTweet);
+    });
   });
 });
--- client/src/components/AddTweetForm.tsx
+++ client/src/components/AddTweetForm.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import {Button, Form, FormGroup, Input} from 'reactstrap';
 
-export class AddTweetForm extends React.Component {
+export class AddTweetForm extends React.Component<{}, {tweet: string}> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      tweet: '',
+    };
+  };
+
+  public handleOnTweetTextChange = (e: any) => {
+    this.setState({tweet: e.target.value});
+  };
+
   public render() {
     return (
-      <div>
-        Add Tweet Form
-      </div>
+      <Form>
+        <FormGroup>
+          <Input 
+            type='textarea'
+            value={this.state.tweet}
+            onChange={this.handleOnTweetTextChange}
+          />
+        </FormGroup>
+        <Button>
+          Howl
+        </Button>
+      </Form>
     );
   };
 };
