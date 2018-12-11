8f107972b7cc19d73de608e3b821adad0f4d0d89
wwSchrader <wwschrader@gmail.com>
2018-12-10 22:05:11 -0800

    Add templating for tweets in the TweetTemplate component

--- client/src/components/TweetTemplate.test.tsx
+++ client/src/components/TweetTemplate.test.tsx
@@ -5,12 +5,30 @@ import {TweetTemplate} from './TweetTemplate';
 
 describe('TweetTemplate', () => {
   let wrapper: ShallowWrapper;
+  let props: any;
 
   beforeEach(() => {
-    wrapper = shallow(<TweetTemplate />);
+    props = {
+      date: Date.now(),
+      tweetMessage: 'This is a tweet message #test @everyone',
+      username: 'someoneSpecial232'
+    }
+    wrapper = shallow(<TweetTemplate {...props}/>);
   });
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render <div />', () => {
+    expect(wrapper.find('div').length).toEqual(3);
+  });
+
+  it('should render <span />', () => {
+    expect(wrapper.find('span').length).toEqual(2);
+  });
+
+  it('should render <b />', () => {
+    expect(wrapper.find('b').length).toEqual(1);
+  });
+
+  it('should render <p />', () => {
+    expect(wrapper.find('p').length).toEqual(2);
   });
 });
--- client/src/components/TweetTemplate.tsx
+++ client/src/components/TweetTemplate.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
-export class TweetTemplate extends React.Component {
+export interface IProps {
+  date: Date,
+  tweetMessage: string,
+  username: string,
+}
+
+export class TweetTemplate extends React.Component<IProps> {
   public render() {
     return (
       <div>
-        TweetTemplate Component
+        <div className='TweetHeader'>
+          <span>
+            <b>{this.props.username}</b>
+            <p>{this.props.date}</p>
+          </span>
+        </div>
+        <div className='TweetBody'>
+          <span>
+            <p>{this.props.tweetMessage}</p>
+          </span>
+        </div>
       </div>
     );
   };
