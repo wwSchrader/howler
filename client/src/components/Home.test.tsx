57b336bcdd5ad5c5836b96683026fc5a98f55155
wwSchrader <wwschrader@gmail.com>
2018-12-12 11:08:56 -0800

    Add TweetTemplate population to Home Component

--- client/src/components/Home.test.tsx
+++ client/src/components/Home.test.tsx
@@ -2,15 +2,48 @@ import {shallow, ShallowWrapper} from 'enzyme';
 import * as React from 'react';
 import '../setupTests';
 import {Home} from './Home';
+import TweetTemplate from './TweetTemplate';
+
+let props: any;
 
 describe('Home', () => {
   let wrapper: ShallowWrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<Home />);
+    props = {
+      tweetArray: [
+        {
+          _id: 'dlsdkjflskjdf',
+          date: Date.now(),
+          message: 'This is a tweet! #yay',
+          ownerId: 'aproductiveuser123'
+        },
+        {
+          _id: 'mifnjjklj',
+          date: Date.now(),
+          message: 'Just another message @everyone',
+          ownerId: 'anotherUser888'
+        },
+      ]
+    }
+    wrapper = shallow(<Home {...props}/>);
   });
 
   it('should render <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render <TweetTemplate />', () => {
+    const passedProps = props.tweetArray.map((prop: any) => {
+      return (
+        <TweetTemplate 
+            key={prop._id}
+            date={prop.date}
+            tweetMessage={prop.message}
+            username={prop.ownerId}
+        />
+      );
+    });
+    expect(wrapper.containsAllMatchingElements(passedProps)).toBe(true);
   });
 });
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import TweetTemplate from './TweetTemplate';
 
-export class Home extends React.Component {
+interface IProps {
+  tweetArray: [{
+    _id: string,
+    date: Date,
+    message: string,
+    ownerId: string
+  }],
+};
+
+export class Home extends React.Component<IProps> {
   public render() {
     return (
       <div>
-        Home component
+        {this.props.tweetArray.map((tweet) => {
+          return (
+            <TweetTemplate
+                key={tweet._id}
+                date={tweet.date}
+                tweetMessage={tweet.message}
+                username={tweet.ownerId}
+            />
+          )
+        })}
       </div>
     );
   };
 };
 
-export default Home;
+const mapStateToProps = (state: any) => {
+  return {
+    tweetArray: state.setTweetArray,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
