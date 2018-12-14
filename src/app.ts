fb56efa0f1098601946a3c2e3dd0631e1b9bb2d2
wwSchrader <wwschrader@gmail.com>
2018-12-13 18:24:30 -0800

    Add pointer for express to react static files

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ const app: express.Application = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.set('port', process.env.PORT || 3000);
 
