45ef4ae9cc8ff42be3fd5f07799c5a262621e0e8
wwSchrader <wwschrader@gmail.com>
2018-12-13 19:38:03 -0800

    Fix static files reference

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app: express.Application = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.set('port', process.env.PORT || 3000);
 
