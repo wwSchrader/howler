4c8ce27a8d81b20fed2d898ac43a0bfd560ff28c
wwSchrader <wwschrader@gmail.com>
2019-07-13 19:24:58 -0700

    Add check sessions to be run at mounting of app component

--- client/src/components/App.test.tsx
+++ client/src/components/App.test.tsx
@@ -2,15 +2,21 @@ import {shallow, ShallowWrapper} from 'enzyme';
 import * as React from 'react';
 import '../setupTests';
 import AddTweetModal from './AddTweetModal';
-import App from './App';
+import {App} from './App';
 import Home from './Home';
 import NavigationBar from './NavigationBar';
 import UserRegOrLoginModal from './UserRegOrLoginModal';
 
 describe('App', () => {
   let wrapper: ShallowWrapper;
+  let props: any;
 
-  beforeEach(() => wrapper = shallow(<App />));
+  beforeEach(() => {
+    props = {
+      checkSession: jest.fn(),
+    };
+    wrapper = shallow(<App {...props}/>)
+  });
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,20 @@
-import * as React from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+import {checkSession} from '../redux/actions/User';
 import AddTweetModal from './AddTweetModal';
 import Home from './Home';
 import NavigationBar from './NavigationBar';
 import UserRegOrLoginModal from './UserRegOrLoginModal';
 
-class App extends React.Component {
+interface IProps {
+  checkSession: () => void,
+};
+
+export class App extends React.Component<IProps> {
+  public componentDidMount() {
+    this.props.checkSession();
+  };
+
   public render() {
     return (
       <div>
@@ -17,4 +27,10 @@ class App extends React.Component {
   };
 };
 
-export default App;
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    checkSession: () => dispatch(checkSession()),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
